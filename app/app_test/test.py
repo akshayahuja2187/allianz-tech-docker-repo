1bec779922cd570a1208d135d86909d9787321d8
Akshay Ahuja <akshay.ahuja2187@gmail.com>
2023-05-18 19:33:37 +0000

    Adding changes for deployment

---
+++ app/app_test/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import pytest
+import requests
+
+@pytest.fixture(scope="session")
+def kube_ip(pytestconfig):
+    return pytestconfig.getoption("ip")
+
+@pytest.mark.rest
+def test_get_request(kube_ip):
+  """Test making a GET request and verifying the output."""
+
+  # Make the request.
+  response = requests.get(f'http://{kube_ip}:30036')
+
+  # Verify the status code.
+  assert response.status_code == 200
+
+  # Verify the content type.
+  assert response.headers['Content-Type'] == 'text/html'
+
+  # Verify the body.
+  assert b'Everything looks good at this point.' in response.content
